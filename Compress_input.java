ff70a2d5712541fa0358930af46d88a1151fca2e
TiewonHenderson <www.ivanhuang123789@gmail.com>
2024-03-20 13:55:48 -0400

    Use Compress_input for Game_system

--- Compress_input.java
+++ Compress_input.java
@@ -25,6 +25,77 @@ import java.util.*;
  */
 public class Compress_input 
 {
-	public static ArrayList<String> input_history_;
+	public static ArrayList<Compress_input> input_history_;
 	
+	public int player_wind_ID;
+	public int decision;
+	public int tile_ID;
+	public ArrayList<Integer> declared_flowers = new ArrayList<Integer>();
+	
+	public Compress_input(String input_str)
+	{
+		/*
+		 * Need player wind ID, decision, tile_ID (single digit ID starts with 0), 2 'f' for flower
+		 * Minimum length of the input_str expected to be 5;
+		 */
+		if(input_str.length() < 5)
+		{
+			
+		}
+		else
+		{
+			int start_flower = 0;
+			/*
+			 *  0 = wind_ID
+			 *  1 = decision
+			 *  2 = tile_ID
+			 *  3 = flower
+			 */
+			int add_mode = 0;
+			for(int i = 0; i < input_str.length(); i++)
+			{
+				if(input_str.charAt(i) == 'f' && start_flower == 0)
+				{
+					start_flower++;
+					add_mode = 3;
+					continue;
+				}
+				switch(add_mode)
+				{
+					case 0:
+						this.player_wind_ID = Integer.parseInt(Character.toString(input_str.charAt(i)));
+						break;
+					case 1:
+						this.decision = Integer.parseInt(Character.toString(input_str.charAt(i)));
+						break;
+					case 2:
+						if((i + 1) < input_str.length())
+						{
+							this.tile_ID = Integer.parseInt(input_str.substring(i, i + 2));
+						}
+						break;
+					case 3:
+						while(input_str.charAt(i) != 'f')
+						{
+							this.declared_flowers.add(Integer.parseInt(Character.toString(input_str.charAt(i))));
+							i++;
+						}
+				}
+				add_mode++;
+			}
+		}
+	}
+	
+	public Compress_input(int wind_ID, int decision, int tile_ID, ArrayList<Integer> declared_flowers)
+	{
+		this.player_wind_ID = wind_ID;
+		this.decision = decision;
+		this.tile_ID = tile_ID;
+		this.declared_flowers = declared_flowers;
+	}
+	
+	public int get_windID(){return this.player_wind_ID;}
+	public int get_decision() {return this.decision;}
+	public int get_tileID() {return this.tile_ID;}
+	public ArrayList<Integer> get_flower_list(){return this.declared_flowers;}
 }
